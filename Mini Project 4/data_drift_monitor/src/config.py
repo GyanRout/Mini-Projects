d94a8074be2ef7e89459cd2ee3b25ee074d8e663
GyanRout <gyanrout224@gmail.com>
2026-04-27 10:39:39 +0530

    Adding all the configuration for the project

---
+++ Mini Project 4/data_drift_monitor/src/config.py
@@ -0,0 +1,6 @@
+REF_DATA_PATH = "../data/reference_data.csv"
+LIVE_BATCH_DATA_PATH = "../data/live_batch_data.csv"
+DRIFT_THRESHOLD_P_VALUE =0.05
+CATEGORICAL_COLUMNS = []
+NUMERICAL_COLUMNS = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup',
+       'Latitude', 'Longitude', 'MedHouseVal']
