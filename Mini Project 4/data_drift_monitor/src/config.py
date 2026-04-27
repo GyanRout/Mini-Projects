42b2a9e8977de169fe6ea30fd92733005cea3706
GyanRout <gyanrout224@gmail.com>
2026-04-27 10:51:58 +0530

    Updated and saved our live data and modified config.py

--- Mini Project 4/data_drift_monitor/src/config.py
+++ Mini Project 4/data_drift_monitor/src/config.py
@@ -3,4 +3,4 @@ LIVE_BATCH_DATA_PATH = "../data/live_batch_data.csv"
 DRIFT_THRESHOLD_P_VALUE =0.05
 CATEGORICAL_COLUMNS = []
 NUMERICAL_COLUMNS = ['MedInc', 'HouseAge', 'AveRooms', 'AveBedrms', 'Population', 'AveOccup',
-       'Latitude', 'Longitude', 'MedHouseVal']
+       'Latitude', 'Longitude']
